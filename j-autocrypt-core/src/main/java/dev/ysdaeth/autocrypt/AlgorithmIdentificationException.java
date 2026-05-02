6f9b921e2faba06667d2bce3e9501015c761eff6
Ysdaeth <189434790+Ysdaeth@users.noreply.github.com>
2026-05-02 10:29:01 +0200

    Alg identification Ex to RuntimeEx

--- j-autocrypt-core/src/main/java/dev/ysdaeth/autocrypt/AlgorithmIdentificationException.java
+++ j-autocrypt-core/src/main/java/dev/ysdaeth/autocrypt/AlgorithmIdentificationException.java
@@ -4,7 +4,7 @@ package dev.ysdaeth.autocrypt;
  * When there is not any known implementation of the algorithm specified by the {@link AlgorithmIdentifier}, i.e:
  * factory does not know type or variant of any matching algorithm implementation for that factory.
  */
-public class AlgorithmIdentificationException extends Exception {
+public class AlgorithmIdentificationException extends RuntimeException {
     /**
      * Creates checked exception, that should be thrown when algorithm identification failed, or is not recognized
      * @param message exception message
